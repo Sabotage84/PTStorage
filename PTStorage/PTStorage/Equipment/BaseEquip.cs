39ecccb6fdac505bdc0308592529170ccd1a5988
IvanZ <sabotage84@mail.ru>
2020-02-15 23:25:03 +0300

    ADD base class

---
+++ PTStorage/PTStorage/Equipment/BaseEquip.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTStorage.Equipment
+{
+    abstract class BaseEquip
+    {
+        
+        public string Name { get => name; set => name = value; }
+        public double Price { get => price; set => price = value; }
+        public string Description { get => description; set => description = value; }
+        public double EntryPrice { get => entryPrice; set => entryPrice = value; }
+        public double Coefficient { get => coefficient; set => coefficient = value; }
+
+        string name;
+        private double price;
+        string description;
+        double entryPrice;
+        double coeffsicient;
+    }
+}
