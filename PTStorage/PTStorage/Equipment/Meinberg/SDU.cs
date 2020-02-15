a56ff3713338c21ab7639349362272349baa42d3
IvanZ <sabotage84@mail.ru>
2020-02-16 00:32:39 +0300

    ADD sdu class

---
+++ PTStorage/PTStorage/Equipment/Meinberg/SDU.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTStorage.Equipment.Meinberg
+{
+    class SDU:BaseEquip
+    {
+        public SDU()
+        {
+
+        }
+        public SDU(string name, string description, double ePrice, double price)
+        {
+            Name = name;
+            Description = description;
+            EntryPrice = ePrice;
+            Price = price;
+            Coefficient = Price / EntryPrice;
+        }
+    }
+}
