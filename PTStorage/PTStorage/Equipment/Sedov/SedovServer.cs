fcd98a8a4c3cb708bd4dcd91cb83bd5b82e5f77d
IvanZ <sabotage84@mail.ru>
2020-02-17 22:28:17 +0300

    ADD Sedov class

--- PTStorage/PTStorage/Equipment/Sedov/SedovServer.cs
+++ PTStorage/PTStorage/Equipment/Sedov/SedovServer.cs
@@ -4,15 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PTStorage.Equipment.Cable
+namespace PTStorage.Equipment.Sedov
 {
-    class Cable:BaseEquip
+    class SedovServer:BaseEquip
     {
-        public Cable()
+        public SedovServer()
         {
-                
+
         }
-        public Cable(string name, string description, double ePrice, double price)
+        public SedovServer(string name, string description, double ePrice, double price)
         {
             Name = name;
             Description = description;
@@ -20,7 +20,5 @@ namespace PTStorage.Equipment.Cable
             Price = price;
             Coefficient = Price / EntryPrice;
         }
-        
-        
     }
 }
