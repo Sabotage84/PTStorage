2d3184891a56582b14be91906bce0c73e55182b1
IvanZ <sabotage84@mail.ru>
2020-02-20 22:45:20 +0300

    ADD Assortment class demo

---
+++ PTStorage/PTStorage/Assortment.cs
@@ -0,0 +1,39 @@
+﻿using PTStorage.Equipment.Antenna;
+using PTStorage.Equipment.Cable;
+using PTStorage.Equipment.Clock;
+using PTStorage.Equipment.Meinberg;
+using PTStorage.Equipment.Sedov;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTStorage
+{
+    class Assortment
+    {
+        List<Server> servsrs;
+        List<Antenna> antennas;
+        List<Cable> cables;
+        List<Clock> clocks;
+        List<Converter> converters;
+        List<Receiver> receivers;
+        List<SDU> sdus;
+        List<SedovServer> sedovServers;
+
+        public Assortment()
+        {
+            servsrs = new List<Server>();
+            antennas = new List<Antenna>();
+            cables = new List<Cable>();
+            clocks = new List<Clock>();
+            converters = new List<Converter>();
+            receivers = new List<Receiver>();
+            sdus = new List<SDU>();
+            sedovServers = new List<SedovServer>();
+
+        }
+
+    }
+}
