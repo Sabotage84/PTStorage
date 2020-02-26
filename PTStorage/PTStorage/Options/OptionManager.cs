14444a8cf3e26c11d2c16a196645aa95473f0d4b
Sabotage84 <sabotage84@mail.ru>
2020-02-26 16:58:11 +0300

    ADD Options not done

---
+++ PTStorage/PTStorage/Options/OptionManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTStorage.Options
+{
+    public static class OptionManager
+    {
+        static List<Option> allOptions;
+        static OptionManager()
+        {
+            AllOptions = new List<Option>();
+        }
+
+        public static List<Option> AllOptions { get => allOptions; set => allOptions = value; }
+    }
+}
