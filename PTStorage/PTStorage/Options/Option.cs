2a86b12239fa19706663d4c78b1f5d28b374ce56
Sabotage84 <sabotage84@mail.ru>
2020-02-26 16:06:47 +0300

    ADD Option class

---
+++ PTStorage/PTStorage/Options/Option.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTStorage.Options
+{
+    public class Option
+    {
+        string shortName;
+        int count;
+        string fullName;
+
+        public string ShortName { get => shortName; set => shortName = value; }
+        public int Count { get => count; set => count = value; }
+        public string FullName { get => fullName; set => fullName = value; }
+
+        public Option()
+        {
+
+        }
+
+        public Option(string name, int count)
+        { 
+            Count = count;
+            if (Count<2)
+                ShortName = name;
+            else
+            {
+                if (ShortName == "LNE" || ShortName == "GE")
+                    FullName = Count + ShortName;
+                else if (ShortName == "TC")
+                    FullName = ShortName + "-" + Count + "-" + Count;
+                else
+                    FullName = ShortName + "-" + Count;
+            }
+           
+        }
+        
+    }
+}
